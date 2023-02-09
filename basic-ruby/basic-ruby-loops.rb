79b4777cced46c15e110f4bd291efd75bec262db
Mason Trepagnier <112119657+MasonT001@users.noreply.github.com>
2023-02-09 13:24:47 -0600

    basic ruby : loops

---
+++ basic-ruby/basic-ruby-loops.rb
@@ -0,0 +1,39 @@
+# Generic Loop 
+#  i = 0
+#  # loop do
+#  # puts "i is #{i}"
+#     i += 1
+#     break if i == 10
+# end
+
+# # While loop
+# # while i < 10 do
+#     # puts "i is #{i}"
+#     # i += 1
+# end
+
+# Until loop
+# until i == 10 do
+#   puts "i is #{i}"
+#   i += 1
+# end
+
+while gets.chomp != "yes" do
+    puts "Will you do the dishes?"
+end
+
+# Ranges
+
+(1..10) # 1, 2, 3, 4, ...
+(1...10)
+('a'..'z')
+
+# For Loop
+# for i in (0..10)
+#     puts "#{i} zombies incoming!"
+# end
+
+# Times loop
+# 5.times do 
+#     puts "Hello World!"
+# end
